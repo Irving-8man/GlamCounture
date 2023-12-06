fe2c042fe46aff9794d6722979a0876701c69fe9
Irving Cupul <geyler0502@outlook.com>
2023-12-06 12:18:19 -0600

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,40 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contacto</h4>
+                <a href="https://www.facebook.com/profile.php?id=61553809957605" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="https://twitter.com/GCounture" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; X </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="https://www.google.com/maps/place/Glam+Couture/@21.1508912,-88.1572126,15z/data=!4m2!3m1!1s0x0:0xee3fd3f5745e470?sa=X&ved=2ahUKEwia37DuvOCCAxUmRTABHa7CD14Q_BJ6BAhREAA&hl=es-419" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                ¡Bienvenido a Glam Couture, tu destino exclusivo para moda y estilo extraordinario! En el corazón de nuestra tienda de ropa y accesorios, descubrirás un mundo donde la elegancia se encuentra con la innovación, y la moda se convierte en una expresión personal única.
+
+Glam Couture es más que una simple tienda; es una experiencia de compras que transforma cada visita en un momento memorable. Nuestro espacio exquisitamente diseñado te sumerge en un ambiente de lujo y glamour, donde cada rincón refleja la sofisticación y la atención al detalle.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Tizimin,Yucatán</p>
+                <p style="font-size:20px" aria-hidden="true">Calle 33 no.351 58/60</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  9871128799</p> <a href="https://web.whatsapp.com/" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                E-mail: <a href="https://outlook.live.com/mail/0/" target="_blank" style="color: #5bc0de"> &nbsp; mikeangelsan@outlook.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">GLAM COUNTURE &copy; 2023</h5>
+</footer>
